dc270d987435af9b2a83694e9acf60b1cfacf9c2
Amna Syeda <amna.syeda98@gmail.com>
2021-07-05 18:47:49 -0400

    code revised

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
-
 const fs = require("fs");
+
 const PORT = process.env.PORT || 3001;
-const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, function() {
     console.log(`App listening on PORT: ${PORT}`);
