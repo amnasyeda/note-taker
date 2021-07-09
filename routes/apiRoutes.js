2a343f5116a9b315a8a355d18e1341b855edf127
Amna Syeda <amna.syeda98@gmail.com>
2021-07-09 19:54:35 -0400

    debug 2

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,31 +1,31 @@
-const fs = require("fs");
-const notesData = require("../db/db.json");
+const fs = require('fs');
+const notesData = require('../db/db.json');
 
 module.exports = function(app){
 
     function writeToDB(notes){
         notes = JSON.stringify(notes);
         console.log (notes);
-        fs.writeFileSync("./db/db.json", notes, function(err){
+        fs.writeFileSync('./db/db.json', notes, function(err){
             if (err) {
                 return console.log(err);
             }
         });
     }
 
-    app.get("/api/notes", function(req, res){
+    app.get('/api/notes', function(req, res){
         res.json(notesData);
     });
 
-    app.post("/api/notes", function(req, res){
+    app.post('/api/notes', function(req, res){
 
         if (notesData.length == 0){
-            req.body.id = "0";
+            req.body.id = '0';
         } else{
             req.body.id = JSON.stringify(JSON.parse(notesData[notesData.length - 1].id) + 1);
         }
         
-        console.log("req.body.id:" + req.body.id);
+        console.log('req.body.id:' + req.body.id);
 
         notesData.push(req.body);
 
@@ -35,7 +35,7 @@ module.exports = function(app){
         res.json(req.body);
     });
 
-    app.delete("/api/notes/:id", function(req, res){
+    app.delete('/api/notes/:id', function(req, res){
         
         let id = req.params.id.toString();
         console.log(id);
@@ -43,7 +43,7 @@ module.exports = function(app){
         for (i=0; i < notesData.length; i++){
            
             if (notesData[i].id == id){
-                console.log("complete!");
+                console.log('complete!');
                 res.send(notesData[i]);
 
                 notesData.splice(i,1);
