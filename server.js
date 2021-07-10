1e9bed863cdde7daa520792712f971b2e234782f
Amna Syeda <amna.syeda98@gmail.com>
2021-07-09 20:01:28 -0400

    application error

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const path = require("path");
+const path = require('path');
 
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
@@ -9,8 +9,8 @@ const htmlRoutes = require('./routes/htmlRoutes');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/routes/apiRoutes');
-app.use('/routes/htmlRoutes');
-app.listen(PORT, function () {
-    console.log(`API server now on port ${PORT}!`);
-  });
+app.use('/', apiRoutes);
+app.use('/', htmlRoutes);
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
