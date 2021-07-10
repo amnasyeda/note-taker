bf042a99cabd94846494427bf565dcc3c38903f6
Amna Syeda <amna.syeda98@gmail.com>
2021-07-10 19:34:21 -0400

    updated server.js

--- server.js
+++ server.js
@@ -4,14 +4,8 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express(); 
 
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
-
-app.use(express.static('public'));
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
-app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
-});
+
+
+app.listen(PORT, () => {  console.log(`API server now on port ${PORT}!`);});
