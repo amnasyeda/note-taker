52046d3650bd20d10b336c614b409056bc9b9093
Amna Syeda <amna.syeda98@gmail.com>
2021-07-10 21:33:34 -0400

    /notes works

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express(); 
 
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
-
 
 app.listen(PORT, () => {  console.log(`API server now on port ${PORT}!`);});
